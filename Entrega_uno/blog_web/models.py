e2ef1ca60f3e420cf04937da8cd5f6de7f611005
jmansicidor <jmansicidor@gmail.com>
2022-05-27 16:55:52 -0300

    Creo class Posteo

--- Entrega_uno/blog_web/models.py
+++ Entrega_uno/blog_web/models.py
@@ -1,3 +1,16 @@
+from ast import AugStore
+from time import time
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    pass
+
+class Posteo(models.Model):
+    autor = models.TextField(max_length=100)
+    titulo = models.TextField(max_length=100)
+    categoria = models.TextField(max_length=100)
+    post = models.TextField(max_length=1000)
+    fecha_posteo = models.DateTimeField(auto_now_add=True)
