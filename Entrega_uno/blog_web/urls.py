e1182d2bb20dd19614fc61c51a8f10a11e35cd72
jmansicidor <jmansicidor@gmail.com>
2022-05-28 18:12:01 -0300

    agrego Class view y html

--- Entrega_uno/blog_web/urls.py
+++ Entrega_uno/blog_web/urls.py
@@ -3,8 +3,12 @@ from blog_web import views
 
 
 urlpatterns = [
-       path("lista", views.bloglist.as_view(), name="blog_list"),
-       path("crear/", views.blogCreate.as_view(), name="crear"),
-       path("detalles/<pk>/",views.BlogDetail.as_view(), name="detalles")
+
+    path("lista", views.Bloglist.as_view(), name="blog_list"),
+    path("crear/", views.BlogCreate.as_view(), name="crear"),
+    path("detalle/<pk>/", views.BlogDetail.as_view(), name ="blog_detail"),
+    path("editar/<pk>/", views.BlogUpdate.as_view(), name ="blog_update"),
+    path("borrar/<pk>/", views.BlogDelete.as_view(), name ="blog_delete"),
+    
 
 ]
--- Entrega_uno/blog_web/views.py
+++ Entrega_uno/blog_web/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from blog_web.models import*
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def detalleposteo(request, id):
     # persona = Persona.objects.get(pk=id)
@@ -15,15 +16,29 @@ def inicio(request):
     blog_web = Posteo.objects.all()
     return render(request, 'inicio.html', {'no_blog': no_blog, 'blog': blog_web})
 
-class bloglist(ListView):
+class Bloglist(ListView):
     model = Posteo
-    template_name = "blog_web/blogView.html"
+    template_name = "blog_web/blogList.html"
 
-class blogCreate(CreateView):
-    model = Posteo
 
 class BlogDetail(DetailView):
 
     model = Posteo
     template_name = "blog_web/blogDetail.html"
 
+class BlogCreate(LoginRequiredMixin, CreateView):
+    model = Posteo
+    uccess_url = reverse_lazy("blogList")
+    fields = ["autor", "titulo", "categoria", "post"]
+
+
+class BlogDelete(LoginRequiredMixin, DeleteView):
+
+    model = Posteo
+    success_url = reverse_lazy("blogList")
+
+class BlogUpdate(LoginRequiredMixin, UpdateView):
+
+    model = Posteo
+    success_url = reverse_lazy("blogList")
+    fields = ["autor", "titulo", "categoria", "post"]
