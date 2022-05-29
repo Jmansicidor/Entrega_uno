95afddfb1d732488a8abf574874f9b7befdd749d
John Cotamo <johncotamo@hotmail.com>
2022-05-29 10:08:06 -0500

    se re organiza codigo con refactor no se modifica nada

--- Entrega_uno/blog_web/urls.py
+++ Entrega_uno/blog_web/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from blog_web import views
 
-
 urlpatterns = [
 
     path("lista", views.Bloglist.as_view(), name="blog_list"),
     path("crear/", views.BlogCreate.as_view(), name="blog_crear"),
-    path("detalle/<pk>/", views.BlogDetail.as_view(), name ="blog_detail"),
-    path("editar/<pk>/", views.BlogUpdate.as_view(), name ="blog_update"),
-    path("borrar/<pk>/", views.BlogDelete.as_view(), name ="blog_delete"),
-    
+    path("detalle/<pk>/", views.BlogDetail.as_view(), name="blog_detail"),
+    path("editar/<pk>/", views.BlogUpdate.as_view(), name="blog_update"),
+    path("borrar/<pk>/", views.BlogDelete.as_view(), name="blog_delete"),
 
 ]
