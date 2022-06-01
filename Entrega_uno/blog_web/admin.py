a102ca57fc5ed5a3f2616d101f6963894d8ad40c
John Cotamo <johncotamo@hotmail.com>
2022-05-31 22:03:24 -0500

    se agrega usuario y persona para prueba

--- Entrega_uno/blog_web/admin.py
+++ Entrega_uno/blog_web/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from blog_web.models import Posteo
+from blog_web.models import Posteo, Persona, Usuario
 
 admin.site.register(Posteo)
+admin.site.register(Persona)
+admin.site.register(Usuario)
 # Register your models here.
